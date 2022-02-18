7c0d229d279363482e8d7cecc28ebac9e2d9d581
Jorge Rollan <jorgerollandiez@gmail.com>
2022-02-18 01:30:17 +0100

    anade los botones a la pantalla al pulsarlos

--- calculator.js
+++ calculator.js
@@ -4,28 +4,52 @@ divButtons.style.display = "grid";
 let number = 0
 for (let a = 3; a > 0; a--) {
     for (let b = 1; b < 4; b++) {
-        number=number+1;
+        number = number + 1;
         const divButton = document.createElement("button");
-        divButton.textContent=number;
-        divButton.style.gridArea=a,a+1,b,b+1;
-        divButton.style.margin="5px"
-        divButtons.appendChild(divButton); 
+        divButton.textContent = number;
+        divButton.style.gridArea = a, a + 1, b, b + 1;
+        divButton.style.margin = "5px"
+        divButton.addEventListener("click", function () {
+            displayButton(divButton);
+        })
+        divButtons.appendChild(divButton);
     }
 }
-    let divButton = document.createElement("button");
-    divButton.textContent=0;
-    divButton.style.gridArea=4,5,1,2;
-    divButton.style.margin="5px"
-    divButtons.appendChild(divButton);
+const divButton = document.createElement("button");
+divButton.textContent = 0;
+divButton.style.gridArea = 4, 5, 1, 2;
+divButton.style.margin = "5px"
+divButton.addEventListener("click", function () {
+    displayButton(divButton);
+})
+divButtons.appendChild(divButton);
 
-    divButton = document.createElement("button");
-    divButton.textContent=",";
-    divButton.style.gridArea=4,5,2,3;
-    divButton.style.margin="5px"
-    divButtons.appendChild(divButton);
+const divButton2 = document.createElement("button");
+divButton2.textContent = ",";
+divButton2.style.gridArea = 4, 5, 2, 3;
+divButton2.style.margin = "5px"
+divButton2.addEventListener("click", function () {
+    displayButton(divButton2);
+})
+divButtons.appendChild(divButton2);
 
-    divButton = document.createElement("button");
-    divButton.textContent="=";
-    divButton.style.gridArea=4,5,3,4;
-    divButton.style.margin="5px"
-    divButtons.appendChild(divButton);
+
+const divButton3 = document.createElement("button");
+divButton3.textContent = "=";
+divButton3.style.gridArea = 4, 5, 3, 4;
+divButton3.style.margin = "5px"
+divButton3.addEventListener("click", function () {
+    displayButton(divButton3);
+})
+divButtons.appendChild(divButton3);
+
+
+function displayButton(buttonValue) {
+    const textDisplay = document.getElementById("textDisplay");
+    if (textDisplay.textContent == "0") {
+        textDisplay.textContent = buttonValue.textContent;
+    }
+    else {
+        textDisplay.textContent = textDisplay.textContent + buttonValue.textContent;
+    }
+}
